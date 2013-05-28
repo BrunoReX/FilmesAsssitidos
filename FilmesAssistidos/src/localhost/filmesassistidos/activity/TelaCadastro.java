6b0863dd3534a48a16fb2c5c5b887667dd2c8f88
Bruno Barbieri <brunorex@gmail.com>
2013-05-28 16:08:50 -0300

    Tela de Cadastro

--- FilmesAssistidos/src/localhost/filmesassistidos/activity/TelaCadastro.java
+++ FilmesAssistidos/src/localhost/filmesassistidos/activity/TelaCadastro.java
@@ -3,12 +3,35 @@ package localhost.filmesassistidos.activity;
 import localhost.filmesassistidos.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Spinner;
 
 public class TelaCadastro extends Activity {
-
+	private EditText edtNome;
+	private EditText edtAno;
+	private EditText edtDiretor;
+	private Spinner spnGenero;
+	private Spinner spnNacionalidade;
+	private RadioButton rbSim;
+	private RadioButton rbNao;
+	private Button btnAdicionar;
+	private Button btnVoltar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_cadastro);
+		
+		edtNome = (EditText) findViewById(R.id.edtNomeCad);
+		edtAno = (EditText) findViewById(R.id.edtAnoCad);
+		edtDiretor = (EditText) findViewById(R.id.edtDiretorCad);
+		spnGenero = (Spinner) findViewById(R.id.spnGeneroCad);
+		spnNacionalidade = (Spinner) findViewById(R.id.spnNacionalidadeCad);
+		rbSim = (RadioButton) findViewById(R.id.rbSimCad);
+		rbNao = (RadioButton) findViewById(R.id.rbNaoCad);
+		btnAdicionar = (Button) findViewById(R.id.btnAdicionarCad);
+		btnVoltar = (Button) findViewById(R.id.btnVoltarCad);
 	}
 }
