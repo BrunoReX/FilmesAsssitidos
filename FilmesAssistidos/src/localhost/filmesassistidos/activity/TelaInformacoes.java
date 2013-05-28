ef5127f49c39a5ba64908b4f086f91c6d3306488
Bruno Barbieri <brunorex@gmail.com>
2013-05-28 17:27:22 -0300

    Tela de Informações

--- FilmesAssistidos/src/localhost/filmesassistidos/activity/TelaInformacoes.java
+++ FilmesAssistidos/src/localhost/filmesassistidos/activity/TelaInformacoes.java
@@ -1,14 +1,39 @@
 package localhost.filmesassistidos.activity;
 
 import localhost.filmesassistidos.R;
+import localhost.filmesassistidos.util.Util;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class TelaInformacoes extends Activity {
-
+	private Util util;
+	
+	private TextView txtTotalFilmes;
+	private TextView txtTotalAssistido;
+	private TextView txtTotalNaoAssistido;
+	private Button btnVoltar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tela_informacoes);
+		
+		util = new Util(this);
+		
+		txtTotalFilmes = (TextView) findViewById(R.id.txtTotalFilmes);
+		txtTotalAssistido = (TextView) findViewById(R.id.txtTotalAssistido);
+		txtTotalNaoAssistido = (TextView) findViewById(R.id.txtTotalNaoAssistido);
+		btnVoltar = (Button) findViewById(R.id.btnVoltarInf);
+		
+		btnVoltar.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				util.voltar();
+			}
+		});
 	}
 }
